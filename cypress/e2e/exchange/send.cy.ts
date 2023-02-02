2fa2de94bdae8b1b0c1df159a682e68dc679abc8
Tien Nam Dao <tien.dao2@tiki.vn>
2023-02-02 10:06:47 +0700

    ci: update default HOST for integration test

--- cypress/e2e/exchange/send.cy.ts
+++ cypress/e2e/exchange/send.cy.ts
@@ -1,16 +1,17 @@
 describe('Send', () => {
+	const HOST = 'http://localhost:3000'
 	it('should redirect', () => {
-		cy.visit('http://localhost/send')
+		cy.visit(`${HOST}/send`)
 		cy.url().should('include', '/swap')
 	})
 
 	it('should redirect with url params', () => {
 		cy.visit(
-			'http://localhost/send?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+			`${HOST}/send?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82`,
 		)
 		cy.url().should(
 			'contain',
-			'http://localhost/swap?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+			`${HOST}/swap?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82`,
 		)
 	})
 })
