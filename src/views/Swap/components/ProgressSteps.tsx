c961bae5437eed01a56d66a2b90a32d8ce97e98f
tiendn <tiendn@stellalab.com>
2023-07-05 14:08:13 +0700

    style: update style in Swap approve step

--- src/views/Swap/components/ProgressSteps.tsx
+++ src/views/Swap/components/ProgressSteps.tsx
@@ -1,4 +1,5 @@
 import { Row } from '@astraprotocol/astra-ui'
+import clsx from 'clsx'
 import { AutoColumn } from 'components/Layout/Column'
 import styles from './styles.module.scss'
 
@@ -23,26 +24,35 @@ export default function ProgressCircles({ steps, disabled = false, ...rest }: Pr
 			<Row style={{ justifyContent: 'space-between', width: '50%' }}>
 				{steps.map((step, i) => {
 					return (
-						// eslint-disable-next-line react/no-array-index-key
 						<div className={styles.circleRow} key={i}>
 							<div
-								className={styles.circle}
-								// confirmed={step}
-								// disabled={disabled || (!steps[i - 1] && i !== 0)}
+								className={clsx(
+									styles.circle,
+									'text-base',
+									disabled || (!steps[i - 1] && i !== 0)
+										? 'contrast-bg-color-10'
+										: 'secondary-bg-color-normal',
+								)}
 							>
 								{step ? '✓' : i + 1}
 							</div>
 							<div
-								className={styles.connector}
-								// prevConfirmed={step}
-								// disabled={disabled}
+								className={clsx(
+									styles.connector,
+									disabled || !steps[steps.length - 1]
+										? 'contrast-bg-color-10'
+										: 'secondary-bg-color-normal',
+								)}
 							/>
 						</div>
 					)
 				})}
 				<div
-					className={styles.circle}
-					// disabled={disabled || !steps[steps.length - 1]}
+					className={clsx(
+						styles.circle,
+						'text-base',
+						disabled || !steps[steps.length - 1] ? 'contrast-bg-color-10' : 'secondary-bg-color-normal',
+					)}
 				>
 					{steps.length + 1}
 				</div>
