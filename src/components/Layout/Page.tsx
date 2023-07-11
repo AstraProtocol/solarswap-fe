54f9b4f82f812ba6844339bae8153c391a73d193
tiendn <tiendn@stellalab.com>
2023-07-11 10:42:28 +0700

    refactor: change file useBUSDPrice to useUSDTPrice

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'contexts/Localization'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { DEFAULT_META, getCustomMeta } from 'config/constants/meta'
-import { useAstraUsdtPrice } from 'hooks/useBUSDPrice'
+import { useAstraUsdtPrice } from 'hooks/useUSDTPrice'
 import { Container } from '@astraprotocol/astra-ui'
 
 // const StyledPage = styled(Container)`
