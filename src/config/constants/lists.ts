4e581b41e8a9b61c65ed7e9deaf8dd4235b6b6e3
tiendn <tiendn@stellalab.com>
2023-07-21 14:09:22 +0700

    fix: default token list + api token list

--- src/config/constants/lists.ts
+++ src/config/constants/lists.ts
@@ -1,19 +1,21 @@
-const SOLAR_EXTENDED = `https://tokens.${process.env.DOMAIN}/solarswap-extended.json`
-const SOLAR_TOP100 = `https://tokens.${process.env.DOMAIN}/solarswap-top-100.json`
-const COINGECKO = `https://tokens.${process.env.DOMAIN}/coingecko.json`
+const SOLAR_DEFAULT = 'https://tokens.solarswap.io/solarswap-default.json'
+const SOLAR_EXTENDED = 'https://tokens.solarswap.io/solarswap-extended.json'
+// const SOLAR_TOP100 = 'https://tokens.solarswap.io/solarswap-top-100.json'
 
 // List of official tokens list
 export const OFFICIAL_LISTS = [] // [SOLAR_EXTENDED, SOLAR_TOP100]
 
 export const UNSUPPORTED_LIST_URLS: string[] = []
+export const WARNING_LIST_URLS: string[] = []
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-	// COINGECKO,
 	// SOLAR_TOP100,
-	// SOLAR_EXTENDED,
+	SOLAR_DEFAULT,
+	SOLAR_EXTENDED,
 	...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
+	...WARNING_LIST_URLS,
 ]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = []
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [SOLAR_EXTENDED]
