4e7aa8030cc8b76fabca3b3731a00b8dd9a5204e
tiendn <tiendn@stellalab.com>
2023-08-16 18:06:34 +0700

    fix: remove saved token

--- src/views/Info/Pools/index.tsx
+++ src/views/Info/Pools/index.tsx
@@ -1,39 +1,15 @@
 import Page from 'components/Layout/Page'
-import { useMemo } from 'react'
 import PoolTable from 'views/Info/components/InfoTables/PoolsTable'
 import { usePoolsData } from '../hooks/usePoolsData'
 import { useTranslation } from 'contexts/Localization'
-import { CHAIN_ID } from 'config/constants/networks'
-import useInfoUserSavedTokensAndPools from 'hooks/useInfoUserSavedTokensAndPoolsList'
-import { usePoolDatasSWR } from 'state/info/hooks'
 import Heading from 'components/Heading'
-import Card from 'components/Card'
 
 const PoolsOverview: React.FC<React.PropsWithChildren> = () => {
 	const { t } = useTranslation()
 	const { poolsData, stableSwapsAprs } = usePoolsData()
-	const { savedPools } = useInfoUserSavedTokensAndPools(parseInt(CHAIN_ID))
-	const watchlistPools = usePoolDatasSWR(savedPools)
-	const watchlistPoolsData = useMemo(
-		() =>
-			watchlistPools.map(pool => {
-				return { ...pool, ...(stableSwapsAprs && { lpApr7d: stableSwapsAprs[pool.address] }) }
-			}),
-		[watchlistPools, stableSwapsAprs],
-	)
 
 	return (
 		<Page>
-			<Heading scale="lg" className="margin-bottom-md">
-				{t('Your Watchlist')}
-			</Heading>
-			<Card>
-				{watchlistPools.length > 0 ? (
-					<PoolTable poolDatas={watchlistPoolsData} />
-				) : (
-					<span className="text text-base">{t('Saved pairs will appear here')}</span>
-				)}
-			</Card>
 			<Heading scale="lg" style={{ marginTop: 40, marginBottom: 16 }} id="info-pools-title">
 				{t('All Pairs')}
 			</Heading>
--- src/views/Info/Tokens/index.tsx
+++ src/views/Info/Tokens/index.tsx
@@ -1,9 +1,6 @@
-import Card from 'components/Card'
 import Heading from 'components/Heading'
 import Page from 'components/Layout/Page'
-import { CHAIN_ID } from 'config/constants/networks'
 import { useTranslation } from 'contexts/Localization'
-import useInfoUserSavedTokensAndPools from 'hooks/useInfoUserSavedTokensAndPoolsList'
 import { useMemo } from 'react'
 import { useAllTokenDataSWR, useTokenDatasSWR } from 'state/info/hooks'
 import TokenTable from 'views/Info/components/InfoTables/TokensTable'
@@ -14,27 +11,14 @@ const TokensOverview: React.FC<React.PropsWithChildren> = () => {
 
 	const allTokens = useAllTokenDataSWR()
 
-	const { savedTokens } = useInfoUserSavedTokensAndPools(parseInt(CHAIN_ID))
-
 	const formattedTokens = useMemo(() => {
 		return Object.values(allTokens)
 			.map(token => token.data)
 			.filter(token => token)
 	}, [allTokens])
-	const watchListTokens = useTokenDatasSWR(savedTokens)
 
 	return (
 		<Page>
-			<Heading scale="lg" className="margin-bottom-md">
-				{t('Your Watchlist')}
-			</Heading>
-			{watchListTokens.length > 0 ? (
-				<TokenTable tokenDatas={watchListTokens} />
-			) : (
-				<Card>
-					<span className="text text-base">{t('Saved tokens will appear here')}</span>
-				</Card>
-			)}
 			<TopTokenMovers />
 			<Heading scale="lg" className="margin-bottom-md margin-top-xl" id="info-tokens-title">
 				{t('All Tokens')}
